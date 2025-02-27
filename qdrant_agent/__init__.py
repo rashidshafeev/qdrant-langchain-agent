f48532f8b526c53a738d9166f84a16c283ca5d07
Rashid Shafeev <rashidshafeev@gmail.com>
2025-02-27 19:21:41 +0300

    Create package structure

---
+++ qdrant_agent/__init__.py
@@ -0,0 +1,11 @@
+"""
+Qdrant LangChain Agent - A tool for interacting with Qdrant vector database.
+
+This module provides functionality to:
+- Create and manage Qdrant collections
+- List collections
+- Add documents to collections
+- Query collections using natural language
+"""
+
+__version__ = "0.1.0"
